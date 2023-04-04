a927f6aeef4c7d61a8d9822b40b31ac9f4a9ded2
Yesol An <79437477+sol2yeet@users.noreply.github.com>
2023-04-04 18:00:46 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// int solution(int n) {   
+//     int answer = n;
+//     while( n <= 1000 && n --> 0){
+//         if(n % 2 == 0 ){
+//             answer += n;
+//                 }
+//             }
+//     return answer;
+// }
+
+int solution(int n) {   
+    int answer = 0;
+    for(int i = 0; i <= n; i++ ){
+        if(i % 2 == 0 ){
+            answer = answer + i;
+                }
+            }
+    return answer;
+}
